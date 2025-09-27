04dbd22d9b32a4116f029774f5026a6cbda8e392
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-09-27 18:17:27 +0000

    commit de controle

--- ex01-express/api/routes/message.js
+++ ex01-express/api/routes/message.js
@@ -1,14 +1,34 @@
 import { v4 as uuidv4 } from "uuid";
 import { Router } from "express";
+import models from "../models";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
+router.get("/", async (req, res) => {
+
+  let {userId} = req.query
+
+  let messages = await models.Message.findAll({
+    where: {
+      userId: userId
+    }
+  })
+  return res.status(200).send(messages);
 });
 
-router.get("/:messageId", (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
+router.get("/:messageId", async (req, res) => {
+
+  let id = req.params.messageId
+  
+  let message = await models.Message.findOne({
+    where: {
+      id: id
+    }
+  })
+
+  if(message == null) return res.status(404).send("A mensagem não foi encontrada")
+
+  return res.status(200).send(message)
 });
 
 router.post("/", (req, res) => {
