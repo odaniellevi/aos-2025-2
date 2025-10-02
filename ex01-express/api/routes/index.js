53c5588010e95b0818c07313b7b636634013fdee
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2025-10-02 22:47:10 +0000

    Fix: Corrige a exportação das rotas no arquivo de índice

--- ex01-express/api/routes/index.js
+++ ex01-express/api/routes/index.js
@@ -2,10 +2,12 @@ import session from "./session.js";
 import user from "./user.js";
 import message from "./message.js";
 import root from "./root.js";
+import tarefa from "./tarefa.js";
 
 export default {
   root,
   session,
   user,
   message,
+  tarefa,
 };
